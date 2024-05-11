d7b33e1648c4e05abff29263298ba172729798e1
thedunncodes <tobioluwa348@gmail.com>
2024-05-11 22:18:32 +0000

    utils

---
+++ utils/redis.js
@@ -0,0 +1,43 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient()
+      .on('error', (err) => {
+        console.error(err);
+      });
+  }
+
+  isAlive() {
+    this.client.ping((err) => {
+      if (err) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  async get(key) {
+    const getProm = promisify(this.client.get).bind(this.client);
+    try {
+      const value = await getProm(key);
+      return value;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async set(key, value, ttl) {
+    this.client.set(key, value, 'EX', ttl);
+  }
+
+  async del(key) {
+    const delProm = promisify(this.client.del).bind(this.client);
+    await delProm(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;

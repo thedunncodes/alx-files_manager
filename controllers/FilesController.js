62879d5dfbbefd5a06c0d1660ca00a8362c2fee0
thedunncodes <tobioluwa348@gmail.com>
2024-07-06 18:31:58 +0100

    FIX: objectid check error

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -99,11 +99,15 @@ export default class FilesController {
     }
 
     const file = await getFileById(req.params.id);
-    if (!(file.userID === user._id)) {
+
+    if (!file) {
       return res.status(404).json({ error: 'Not found' });
     }
 
-    return res.status(201).json(file);
+    if (!(file.userId.toString() === user._id.toString())) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+    return res.status(200).json(file);
   }
 
   static async getIndex(req, res) {
@@ -120,6 +124,6 @@ export default class FilesController {
 
     const userFiles = await getUserFilesWithId(user._id, page);
 
-    return res.status(201).json(userFiles);
+    return res.status(200).json(userFiles);
   }
 }

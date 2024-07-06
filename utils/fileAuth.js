cd92e8b86d244430a923499f2a5a87c59ede213d
thedunncodes <tobioluwa348@gmail.com>
2024-07-06 01:43:20 +0100

    UPDATE: added a new function 'getUserFilesWithId'

--- utils/fileAuth.js
+++ utils/fileAuth.js
@@ -1,7 +1,7 @@
 import { ObjectId } from 'mongodb';
 import dbClient from './db';
 
-export default async function getFileById(id) {
+export async function getFileById(id) {
   if (!id) {
     return null;
   }
@@ -10,4 +10,16 @@ export default async function getFileById(id) {
     return null;
   }
   return file;
+}
+
+export async function getUserFilesWithId(Id) {
+  if (!Id) {
+    return null;
+  }
+  const user = await dbClient.client.db().collection('files').find({ userId: new ObjectId(Id) }).toArray();
+  if (!user) {
+    return null;
+  }
+
+  return user;
 }

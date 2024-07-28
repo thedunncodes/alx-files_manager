c7f088ab7352085eea0f4e137d38c374914cd355
thedunncodes <tobioluwa348@gmail.com>
2024-07-28 16:40:54 +0100

    UPDATE: added a new 'GET /files/:id/data' route

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,6 @@ router.put('/files/:id/publish', FilesController.putPublish);
 
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
+router.get('/files/:id/data', FilesController.getFile);
+
 module.exports = router;
